bdac345d1ec7bb65a99154fad4fb58f05ec0f474
HugeBrain16 <hugebrain16@gmail.com>
2023-02-17 18:47:39 +0700

    omits the .git from repo name
    shorter name lol

--- setup.py
+++ setup.py
@@ -6,7 +6,5 @@ setup(
     description="An INI/LTX config parser",
     py_modules=["iniltx"],
     url="https://github.com/hugebrain16/iniltx",
-    install_requires=[
-        "iniparser @ git+https://github.com/HugeBrain16/iniparser.git@1.1.2"
-    ],
+    install_requires=["iniparser @ git+https://github.com/HugeBrain16/iniparser@1.1.2"],
 )
