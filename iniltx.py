968bdf095768ee92cab155c265d286b98a243ae3
HugeBrain16 <hugebrain16@gmail.com>
2023-02-18 10:27:30 +0700

    implement interpolation
    (super messy)

--- iniltx.py
+++ iniltx.py
@@ -8,6 +8,7 @@ _RE_MACRO = re.compile(r"^\#(?P<name>[a-zA-Z_]+)\s+(?P<value>.+)$")
 _RE_INHERIT = re.compile(
     r"^\[(?P<section>[a-zA-Z0-9_-]+)\]\:(?P<instance>[a-zA-Z0-9_-]+)$"
 )
+_RE_INTERP = re.compile(r"\%([a-zA-Z0-9_:-]+)\%")
 
 
 def _parse_macro(string: str):
@@ -22,6 +23,10 @@ def _parse_inherit(string: str):
 
     if inh:
         return inh.groups()
+
+
+def _get_interps(string: str):
+    return _RE_INTERP.findall(string)
 
 
 def _tokenize(string: str | io.StringIO):
@@ -45,6 +50,68 @@ def _tokenize(string: str | io.StringIO):
             result.append(["ltx", line, lineno])
 
     return result
+
+
+def _get_itpsec(string: str):
+    result = [None, None]
+    tokens = string.split(":", 1)
+
+    if len(tokens) == 2:
+        result[0] = tokens[0]
+        result[1] = tokens[1]
+    else:
+        result[0] = tokens[0]
+
+    return result
+
+
+def _parse_interps(config: dict):
+    def assign_value(option: str, section: str = None):
+        for itp in _get_interps(config[section][option] if section else config[option]):
+            tokens = _get_itpsec(itp)
+
+            if tokens[1]:
+                if not isinstance(config.get(tokens[0]), dict):
+                    raise ValueError("Section doesn't exist: " + tokens[0])
+
+                if tokens[1] not in config[tokens[0]]:
+                    raise ValueError(
+                        f"Option '{tokens[1]}' doesn't exist in section '{tokens[0]}'"
+                    )
+                elif config[tokens[0]].get(tokens[1]) is None:
+                    raise ValueError("referencing a value only option: " + itp)
+
+                if section:
+                    config[section][option] = config[section][option].replace(
+                        f"%{itp}%", config[tokens[0]][tokens[1]]
+                    )
+                else:
+                    config[option] = config[option].replace(
+                        f"%{itp}%", config[tokens[0]][tokens[1]]
+                    )
+            else:
+                if tokens[0] not in config:
+                    raise ValueError(f"Option doesn't exist: " + tokens[0])
+                elif isinstance(config[tokens[0]], dict):
+                    raise ValueError(f"referencing a section: " + itp)
+                elif config.get(tokens[0]) is None:
+                    raise ValueError("referencing a value only option: " + itp)
+
+                if section:
+                    config[section][option] = config[section][option].replace(
+                        f"%{itp}%", config[tokens[0]]
+                    )
+                else:
+                    config[option] = config[option].replace(
+                        f"%{itp}%", config[tokens[0]]
+                    )
+
+    for opt in config:
+        if isinstance(config[opt], dict):
+            for sopt in config[opt]:
+                assign_value(sopt, opt)
+        else:
+            assign_value(opt)
 
 
 def parse(tokens: list[list[str]]):
@@ -111,4 +178,5 @@ def parse(tokens: list[list[str]]):
     if inherit and inherit_segment:
         result[inherit].update(iniparser.getall(inherit_segment))
 
+    _parse_interps(result)
     return result
